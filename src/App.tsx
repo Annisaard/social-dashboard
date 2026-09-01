9cd897d7f75568c08d30dbd97936d6990bbc4808
fadhil <annisarahmadhani78@gmail.com>
2026-09-01 21:36:19 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center">
+        <h1 className="text-4xl font-bold">Social Dashboard</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
